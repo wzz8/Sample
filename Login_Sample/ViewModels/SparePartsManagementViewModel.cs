83fa1cee23d26a593a92fcd4ee428d869d29ea87
wzz8 <wzz8@msn.com>
2025-12-21 17:20:13 +0800

    update spare parts management layout

--- Login_Sample/ViewModels/SparePartsManagementViewModel.cs
+++ Login_Sample/ViewModels/SparePartsManagementViewModel.cs
@@ -1,5 +1,11 @@
+using System;
+using System.Collections.Generic;
 using System.ComponentModel;
+using System.Linq;
 using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Input;
 using MaterialDesignThemes.Wpf;
 
@@ -7,19 +13,66 @@ namespace Login_Sample.ViewModels
 {
     public class SparePartsManagementViewModel : INotifyPropertyChanged
     {
-        public event PropertyChangedEventHandler? PropertyChanged;
-
-        private void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        // 统计卡片属性
+        private int _totalSpareParts;
+        public int TotalSpareParts
         {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            get { return _totalSpareParts; }
+            set { _totalSpareParts = value; OnPropertyChanged(); }
+        }
+
+        private int _totalInventory;
+        public int TotalInventory
+        {
+            get { return _totalInventory; }
+            set { _totalInventory = value; OnPropertyChanged(); }
+        }
+
+        private int _todayInbound;
+        public int TodayInbound
+        {
+            get { return _todayInbound; }
+            set { _todayInbound = value; OnPropertyChanged(); }
+        }
+
+        private int _todayOutbound;
+        public int TodayOutbound
+        {
+            get { return _todayOutbound; }
+            set { _todayOutbound = value; OnPropertyChanged(); }
+        }
+
+        // 导航相关属性和命令
+        private object _currentView;
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set { _currentView = value; OnPropertyChanged(); }
         }
 
         // 构造函数
         public SparePartsManagementViewModel()
         {
-            // 初始化数据或命令
+            // 初始化统计数据
+            InitializeDashboardData();
         }
 
-        // 这里可以添加备件管理相关的属性和命令
+        // 初始化仪表盘数据
+        private void InitializeDashboardData()
+        {
+            // 这里可以从数据库获取真实数据
+            // 暂时使用模拟数据
+            TotalSpareParts = 1250;
+            TotalInventory = 5870;
+            TodayInbound = 32;
+            TodayOutbound = 18;
+        }
+
+        // 属性变更通知实现
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
--- Login_Sample/Views/SparePartsManagementView.xaml.cs
+++ Login_Sample/Views/SparePartsManagementView.xaml.cs
@@ -1,17 +1,37 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Controls;
+using System.Windows; 
+using System.Windows.Input;
 
 namespace Login_Sample.Views
 {
-    public partial class SparePartsManagementView : UserControl
+    public partial class SparePartsManagementView
     {
         public SparePartsManagementView()
         {
             InitializeComponent();
+        }
+
+        private void Card_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            // 获取卡片的Tag值，即导航目标页面
+            string pageName = ((FrameworkElement)sender).Tag.ToString();
+
+            // 获取MainWindow实例
+            var mainWindow = Application.Current.MainWindow as MainWindow;
+            if (mainWindow != null)
+            {
+                // 获取MainViewModel实例
+                var viewModel = mainWindow.DataContext as ViewModels.MainViewModel;
+                if (viewModel != null && viewModel.NavigateCommand.CanExecute(pageName))
+                {
+                    // 调用导航命令
+                    viewModel.NavigateCommand.Execute(pageName);
+                }
+            }
         }
     }
 }
